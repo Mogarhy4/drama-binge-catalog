07ae363133b604a3dc9a396d29f7590a420b9ba1
Mogarhy4 <eng.mohamedelgarhy4@gmail.com>
2026-09-14 21:43:07 +0300

    montioring get

--- bot.py
+++ bot.py
@@ -19,6 +19,11 @@ class HealthCheckHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"Bot is alive!")
+
+    def do_HEAD(self):
+        self.send_response(200)
+        self.end_headers()
+
     def log_message(self, format, *args):
         pass  # Keeps logs clean
 
