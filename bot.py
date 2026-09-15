3bfd0096950cbe35d84a3303e356091c27ab5538
Mogarhy4 <eng.mohamedelgarhy4@gmail.com>
2026-09-15 20:54:40 +0300

    push new episods

--- bot.py
+++ bot.py
@@ -51,7 +51,15 @@ DATABASE = {
                     {"name": "Episode 6", "file_id": "BAACAgQAAxkBAAM0aqXK3L0jdGngklPnpaIq-IerCUwAAvYgAAJx7jBRYQHen2xEnQs9BA"},
                     {"name": "Episode 7", "file_id": "BAACAgQAAxkBAAM2aqXLCtYRNZ5hKu5oBmqV-7vPWxcAAvcgAAJx7jBR5t580ZWkbXM9BA"},
                     {"name": "Episode 8", "file_id": "BAACAgQAAxkBAAM4aqXLKufSQNg8wbFmw_LVQ1MpFj8AAvggAAJx7jBRQdT--Oayq-M9BA"},
-                    {"name": "Episode 9", "file_id": "BAACAgQAAxkBAAM6aqXLSETumJjVn4INiTOVZLKWBR8AAvkgAAJx7jBRZ1qxRCKbOu89BA"}
+                    {"name": "Episode 9", "file_id": "BAACAgQAAxkBAAM6aqXLSETumJjVn4INiTOVZLKWBR8AAvkgAAJx7jBRZ1qxRCKbOu89BA"},
+                    {"name": "Episode 10", "file_id": "BAACAgQAAxkBAAID1WqphBlntSuESMQM1J8Er9nNqXseAAJjIAACSd9JUf6UMtv0YN6UPQQ"},
+                    {"name": "Episode 11", "file_id": "BAACAgQAAxkBAAID1mqphBnLnqHOCtMAATXgjDrTu-OwxQACZSAAAknfSVEIwXTVwn3k7T0E"},
+                    {"name": "Episode 12", "file_id": "BAACAgQAAxkBAAID2WqphUIT56W3Qb1-KkPxXtWmdSL1AAJnIAACSd9JUVVYPeG48vRAPQQ"},
+                    {"name": "Episode 13", "file_id": "BAACAgQAAxkBAAID22qphUKYMfsCO-y5RKX_ymJrkRs3AAJqIAACSd9JUX8i5BCVm_65PQQ"},
+                    {"name": "Episode 14", "file_id": "BAACAgQAAxkBAAID22qphUKYMfsCO-y5RKX_ymJrkRs3AAJqIAACSd9JUX8i5BCVm_65PQQ"},
+                    {"name": "Episode 15", "file_id": "BAACAgQAAxkBAAID3GqphULVTjxhQxeaNb2AI_d7fZaQAAJrIAACSd9JUeBW9TWVphVDPQQ"},
+                    {"name": "Episode 16", "file_id": "BAACAgQAAxkBAAID3WqphULmDA5iVp3x-xfJXZfP4EPOAAJsIAACSd9JUa393RZhwwYgPQQ"},
+                    {"name": "Episode 17", "file_id": "BAACAgQAAxkBAAID3mqphULuK80w-Mudv3TzjfkwrnZjAAJtIAACSd9JUb1R62oJ2gskPQQ"}
                 ]
             }
         ]
